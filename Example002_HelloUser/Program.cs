7c7a58d856dda2d337c30e0344d9d06b0f0bd77a
Pavel Ostapchuk <paviv84@gmail.com>
2022-11-11 14:36:15 +0300

    add Example002_HelloUser

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -2,3 +2,7 @@
 string username = Console.ReadLine();
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
+Console.Write("Enter your name ");
+String username1 = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username1);
